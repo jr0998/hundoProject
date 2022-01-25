1df5bef90d9aaa1c7dd163be596e0a8f263c6333
jr0998 <33519065+jr0998@users.noreply.github.com>
2022-01-25 17:43:06 -0500

    first JS commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-//call hello world
-function helloWorld(){
-    alert("Hello World");
+//get values from webpage
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+ 
+
+    //validates user input as integers
+    startValue.parseInt(startValue);
+    endValue.parseInt(endValue);
+ 
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+
+        //call generateNumber()    
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers()
+    
+        displayNumbers(numbers);
+    }
+    else{
+        //alert message to get corrective action and prevent error
+        alert("Input not an Integer, enter correct value")
+    }
+
+    
+}
+//generate number from start value to end value
+function generateNumbers(start, end){
+    let numbers = [];
+    //populate a dynamic array from any two start and end values given by user
+    for(let i = start; i<=end; i++){
+        numbers.push(i)
+    }
+    return numbers;
+}
+
+//display all numbers and even numbers in Bold
+function displayNumbers(num){
+    let templateRows = "";
+    for(let i = 0;i<num.length; i++){
+        let number = num[i];
+        templateRows+= `<tr><td>${number}</td></tr>`;
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
